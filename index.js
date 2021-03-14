428e55144c9762a86c96f312d302703b76fa59dd
ngoctho <hangoctho2502@gmail.com>
2021-03-14 14:34:44 +0700

    make it better

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require("express");
-const morgan = require("morgan");
 const app = express();
-const port = 3000;
 
+// use morgan logger
+const morgan = require("morgan");
 app.use(morgan("combined"));
+// set public static folder
 app.use(express.static(__dirname + "/public"));
 
+// use view engine
 const expressHbs = require("express-handlebars");
 const hbs = expressHbs.create({
   extname: "hbs",
   defaultLayout: "layout",
-  layoutsDir: __dirname + "/views/layouts",
-  partialsDir: __dirname + "/views/partials",
+  layoutsDir: __dirname + "/views/layouts/",
+  partialsDir: __dirname + "/views/partials/",
 });
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 
+// define routers
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -24,10 +27,15 @@ app.get("/:page", (req, res) => {
     blog: "Our Blog",
     category: "Shop Category",
     cart: "Shopping Cart",
+    register: "Register",
+    tracking: "Order Tracking",
   };
   let page = req.params.page;
   res.render(page, { banner: banners[page] });
 });
+
+// set server port & start server
+const port = 3000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
