fc6e094cb502a72f3bfa3f8de363f68f0a10745a
ngoctho <hangoctho2502@gmail.com>
2021-03-14 13:36:25 +0700

    update

--- README.md
+++ README.md
@@ -8,16 +8,28 @@
 ## Running Locally
 
 ```npm
-npm init
-npm install -s express
-npm install nodemon --save-dev
+$ npm init
+$ npm install -s express
+```
+
+Automatically restarting application
+
+```npm
+$ npm install nodemon --save-dev
 ```
 Add to scripts in package.json
 
 ```json
-
 "start": "nodemon --inspect index.js"
+```
+HTTP request logger middleware for node.js
 
+```npm
+$ npm install morgan --save-dev
 ```
 
-## 
+Express Handlebars view engine
+
+```npm
+$ npm install express-handlebars
+```
--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(morgan("combined"));
+app.use(express.static(__dirname + "/public"));
 
+const expressHbs = require("express-handlebars");
+const hbs = expressHbs.create({
+  extname: "hbs",
+  defaultLayout: "layout",
+  layoutsDir: __dirname + "/views/layouts",
+  partialsDir: __dirname + "/views/partials",
+});
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/:page", (req, res) => {
+  let banners = {
+    blog: "Our Blog",
+    category: "Shop Category",
+    cart: "Shopping Cart",
+  };
+  let page = req.params.page;
+  res.render(page, { banner: banners[page] });
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
